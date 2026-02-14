30f627a305eea6e95945fe1e36379896327bfa2c
Anvi200412 <anvigadhiya@gmail.com>
2026-02-14 17:38:35 +0530

    updated files

--- src/Pages/Pagecontext.jsx
+++ src/Pages/Pagecontext.jsx
@@ -1,44 +1,4 @@
-// import { createContext, useEffect, useState } from "react";
 
-// export const ProductContext = createContext();
-// export const ProductProvider = ({ children }) => {
-//   const [products, setProducts] = useState([]);
-//   const [categories, setCategories] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         setLoading(true);
-//         const [prodRes, catRes] = await Promise.all([
-//           fetch("https://dummyjson.com/products?limit=900"),
-//           fetch("https://dummyjson.com/products/categories"),
-//         ]);
-
-//         const prodData = await prodRes.json();
-//         const catData = await catRes.json();
-
-//         setProducts(prodData.products);
-//         setCategories(catData);
-
-//       }
-//       catch (error) {
-//         console.error(error);
-//       }
-//       finally {
-//         setLoading(false);
-//       }
-//     }
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <ProductContext.Provider value={{ products, categories, loading }}>
-//       {children}
-//     </ProductContext.Provider>
-//   );
-// };
 
 import { createContext, useEffect, useState } from "react";
 
